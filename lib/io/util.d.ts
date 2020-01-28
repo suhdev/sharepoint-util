62d248331d95c4e98a70b42b5e7f0084b8499b92
Michael Smith <mike@cadmia.io>
2020-01-28 16:33:23 +0000

    Updated tsc lib files

--- lib/io/util.d.ts
+++ lib/io/util.d.ts
@@ -1,2 +1,2 @@
-export declare function mkdirp(path: string): Promise<{}>;
+export declare function mkdirp(path: string): Promise<unknown>;
 export declare function createDirectoryIfNotExist(path: string, operation?: string): Promise<void>;
